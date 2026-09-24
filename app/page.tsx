ffa1fc16cb7699b3bafdd50c8b80e387422e090b
Imran Hossain <imranhossen0586@gmail.com>
2026-09-24 12:24:09 +0600

    fix: add responsive padding to main container in HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import SkillsSection from "./(public)/Skills/SkillsSection"
 
 export default function HomePage() {
   return (
-    <main>
+    <main className="px-0 md:px-6 lg:px-0">
       <HeroSection />
       <AboutSection />
       <SkillsSection />
